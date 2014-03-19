032e3c9f799ebe20b3dfe21de4cdced1bd8e8941
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2014-03-19 00:22:10 +0000

    Recursive stack popping implemented - kinda working still...

--- src/machinelearning/Test.java
+++ src/machinelearning/Test.java
@@ -51,41 +51,61 @@ public class Test
 
         //System.out.println(examples.size());
         g.addVertex(examples);
-        addFeatToGraph(examples, feats.pop());
+
+        addFeatToGraph(examples, feats, featToNewValueStack(feats.peek()));
 
         //Visualise what we've added so far
         JGraphFrame jGraphFrame = new JGraphFrame(g);
     }
 
-    private void addFeatToGraph(ExampleSet parent, String feat)
+    private Stack<String> featToNewValueStack(String feat)
     {
+        Stack<String> valueStack = new Stack<>();
+
         LinkedHashSet<String> allCurrentFeatValues = values.get(feat);
-        
-        for (String currentValue : allCurrentFeatValues)
+
+        for (String currentFeatValue : allCurrentFeatValues)
         {
-            //System.out.println("\ncurrentValue: " + currentValue);
-            ExampleSet currentIteration = parent.getExamples(feat, currentValue);
-
-            g.addVertex(currentIteration);
-            g.addEdge(parent, currentIteration, feat + ": " + currentValue);
-
-            if (feats.size() > 2)
-            {
-                String nextFeat;
-                //If last value
-                if (currentValue.equals( allCurrentFeatValues.toArray()[allCurrentFeatValues.size()-1] ) )
-                {
-                    nextFeat = feats.pop();
-                }
-                else
-                {
-                    nextFeat = feat;
-                }
-                addFeatToGraph( currentIteration, nextFeat );
-            }
-
-            //currentIteration.printProbabilityInfo();
+            valueStack.push(currentFeatValue);
         }
+
+        return valueStack;
+    }
+
+    private void addFeatToGraph(ExampleSet parent, Stack<String> featStack, Stack<String> valueStack)
+    {
+        if (valueStack.isEmpty())
+        {
+            //Advance to next Feature
+            String newFeat = null;
+            try
+            {
+                newFeat = featStack.pop();
+            }
+            catch (EmptyStackException e)
+            {
+                return;
+            }
+            valueStack = featToNewValueStack(newFeat);
+            addFeatToGraph(parent, featStack, valueStack);
+            return;
+        }
+
+        if (featStack.size() < 6)
+        {
+            //End of recursion
+            return;
+        }
+
+        String newValue = valueStack.pop();
+
+        ExampleSet currentIteration = parent.getExamples(featStack.peek(), newValue);
+
+        g.addVertex(currentIteration);
+        g.addEdge(parent, currentIteration, featStack.peek() + ": " + newValue);
+
+        addFeatToGraph(parent, featStack, valueStack);
+
     }
 
 }
